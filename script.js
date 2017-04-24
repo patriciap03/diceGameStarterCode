801f86769d34a13890f2463be6320a0bbea112f3
Patricia Perez <patricia.perez0301@gmail.com>
2017-04-24 13:41:30 +0000

    did it

--- script.js
+++ script.js
@@ -2,7 +2,33 @@ $(document).ready(function(){
   function rolldice(){
   //Place your code here 
 
-
   };
+  
+  $("#roll-dice").click(function(){ 
+  var dice1 = Math.random()*6;
+  var dice2 = Math.floor(dice1+1);
+  
+console.log(dice2);
+  $("#first-die").removeClass();
+  if(dice2===1){
+    $("#first-die").addClass("dice-1");
+  }
+  else if(dice2===2){
+    $("#first-die").addClass("dice-2");
+  }
+   else if(dice2===3){
+    $("#first-die").addClass("dice-3");
+  }
+   else if(dice2===4){
+    $("#first-die").addClass("dice-4");
+  }
+   else if(dice2===5){
+    $("#first-die").addClass("dice-5");
+  }
+   else if(dice2===6){
+    $("#first-die").addClass("dice-6");
+  }
+  
+});
   
 });
